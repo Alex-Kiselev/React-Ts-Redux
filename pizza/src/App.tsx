602b7905874f8f798195a7250105d97222c90fe2
AA_KISELEV1 <AA_KISELEV1@protek.ru>
2024-01-30 00:14:38 +0300

    11.6. Упражнение - Создание Layout + router + Link

--- pizza/src/App.tsx
+++ pizza/src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -13,7 +17,8 @@ function App() {
 		<>
 			<Button onClick={addCounter}>Кнопка</Button>
 			<Button appearence='big' onClick={addCounter}>Кнопка</Button>
-			<Input />
+			<Input placeholder='Email' />
+
 		</>
 	);
 }
