0adb9084dac62adf1bb336f4eb384e083f215cb7
AA_KISELEV1 <AA_KISELEV1@protek.ru>
2024-01-30 00:34:42 +0300

    11.8. NavLink - Круче обычного Link
    1) использует под капотом cb active для подсвечивания навигации
    <NavLink to='/' className={({ isActive })

--- pizza/src/layout/Menu/Layout.tsx
+++ pizza/src/layout/Menu/Layout.tsx
@@ -1,19 +1,9 @@
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import {  NavLink, Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
-import { useEffect } from 'react';
 import cn from 'classnames';
 
 export function Layout() {
-
-	const location = useLocation();
-	
-	
-	useEffect(() => {
-		console.log('🚀 ~ Layout ~ location:', location);
-	
-	}, [location]);
-	
 
 	return <div className={styles['layout']}>
 		<div className={styles['sidebar']}>
@@ -23,13 +13,15 @@ export function Layout() {
 				<div className={styles['email']}>alari@ya.ru</div>
 			</div>
 			<div className={styles['menu']}>
-				<Link to='/' className={cn(styles['link'], {
-					[styles.active] : location.pathname === '/'
+				<NavLink to='/' className={({ isActive }) => cn(styles['link'], {
+					[styles.active]: isActive
 				})}>
 					<img src="/menu-icon.svg" alt="Иконка меню" />
-					Меню</Link>
-				<Link to='/cart' className={styles['link']}>
-					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</Link>
+					Меню</NavLink>
+				<NavLink to='/cart' className={({ isActive }) => cn(styles['link'], {
+					[styles.active]: isActive
+				})}>
+					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</NavLink>
 			</div>
 			<Button className={styles['exit']}>
 				<img src="/exit-icon.svg" alt="Иконка выхода" />
