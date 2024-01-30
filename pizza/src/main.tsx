c46c7a0afd2848cf3af18b26ebc47dd241a8ba59
AA_KISELEV1 <AA_KISELEV1@protek.ru>
2024-01-31 00:35:53 +0300

    12.7. errorElement +  (12.8. lazy + 12.9 Suspense)
    1) errorElement указывается в роутах обработка ошибок показывает эелемент и обрабатывает ошибку
    2) Lazy ленивая загрузка чтобы не загружать бандл при сборке, нужно для того чтобы не грузит лишние файлы если пользователь не зайдет в этот или иной компонент
    3) LAZY + SUSPENSE

--- pizza/src/main.tsx
+++ pizza/src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Cart } from './pages/Cart/Cart.tsx';
-import { Menu } from './pages/Menu/Menu.tsx';
-import { Error } from './pages/Error/Error.tsx';
+import { Error as ErropPage } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Menu/Layout.tsx';
 import { Product } from './pages/Product/Product.tsx';
 import axios from 'axios';
 import { PREFIX } from './helpers/API.ts';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
 	{
@@ -17,7 +18,7 @@ const router = createBrowserRouter([
 		children: [
 			{
 				path: '/',
-				element: <Menu />
+				element: <Suspense fallback={<>Загрузка...</>}><Menu /></Suspense>
 			},
 			{
 				path: '/cart',
@@ -26,6 +27,7 @@ const router = createBrowserRouter([
 			{
 				path: '/product/:id',
 				element: <Product />,
+				errorElement: <>Ошибка</>,
 				loader: async ({ params }) => {
 					await new Promise<void>((resolve) => {
 						setTimeout(() => {
@@ -40,7 +42,7 @@ const router = createBrowserRouter([
 	},
 	{
 		path: '*',
-		element: <Error />
+		element: <ErropPage />
 	}
 ]);
 
